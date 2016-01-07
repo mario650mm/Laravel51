38a7093a3a42b9b17e9f4611f946b34e231e1f21
mario650mm <mario650m@gmail.com>
2016-01-07 13:54:39 -0600

    Add new views and lang on Laravel by Mario

---
+++ Laravel51/resources/views/generals/errors.blade.php
@@ -0,0 +1,14 @@
+@if ($errors->any())
+    <div class="alert alert-danger" role="alert">
+        <button type="button" class="close" data-dismiss="alert">
+            <span aria-hidden="true">&times;</span>
+            <span class="sr-only">Close</span>
+        </button>
+        <p>@yield('title_error')</p>
+        <ul>
+            @foreach($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
