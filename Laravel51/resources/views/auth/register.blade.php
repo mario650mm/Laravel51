caadd42ef408041248c8e631d4f3b8654a699af9
mario650mm <mario650m@gmail.com>
2016-01-20 18:42:43 -0600

    Modify the project by Mario

--- Laravel51/resources/views/auth/register.blade.php
+++ Laravel51/resources/views/auth/register.blade.php
@@ -50,6 +50,13 @@
                             </div>
 
                             <div class="form-group">
+                                <label class="col-md-4 control-label">User type</label>
+                                <div class="col-md-6">
+                                    <input type="select" class="form-control" name="role">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
                                 <div class="col-md-6 col-md-offset-4">
                                     <button type="submit" class="btn btn-primary">
                                         Register
